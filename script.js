543edee9039c7277aea3f3eb2f0d615de41581ba
Joseph Bacal <bacalj@me.com>
2013-12-22 22:42:16 -0500

    fixedfor now by building selector at creation

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ $(document).ready(function(){
 	$("#add").click(function(){
 		var my_text = $("#entry").val();
 		var my_item = $('<div class="food"></div>').text(my_text);
-		$("#paper").append(my_item);
+		my_item.on('click', function(){
+			$(this).toggleClass("checked_food");
+		});
+		$("#wrap").append(my_item);
 	});
 
-	$('.food').click(function(){
-		$(this).toggleClass("checked_food");
+		$("div.food").click(function(){
+			$(this).toggleClass("checked_food");
 	});
 });
 
