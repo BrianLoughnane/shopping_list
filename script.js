a88f9178a631ccd9813adbab6389d2bec8ec930e
Joseph Bacal <bacalj@me.com>
2013-12-23 19:54:40 -0500

    added double click, still working on error trapping

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-
+	
 	$("#add").click(function(){
 		var my_text = $("#entry").val();
 		var my_item = $('<div class="food"></div>').text(my_text);
-		my_item.on('click', function(){
-			$(this).toggleClass("checked_food");
-		});
+
+			my_item.on('click', function(){
+				$(this).toggleClass("checked_food");
+			});
+
+			my_item.on('dblclick', function(){
+				$(this).hide();
+			});
+		
 		$("#wrap").append(my_item);
 	});
 
+		//this is so the first item behaves like the rest
 		$("div.food").click(function(){
 			$(this).toggleClass("checked_food");
-	});
+		});
+
+		$("div.food").dblclick(function(){
+			$(this).hide();
+		});
 });
 
+//
