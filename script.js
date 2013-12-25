5591414aba10524775dbef4625ca1e1c48ad7afd
Joseph Bacal <bacalj@me.com>
2013-12-25 14:30:15 -0500

    fixed it so enter key works - next step is to delete text in box after it has been transfered to item

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
-	
 	$("#add").click(function(){
 		var my_text = $("#entry").val();
 		var my_item = $('<div class="food"></div>').text(my_text);
+
+		my_item.on('click', function(){
+			$(this).toggleClass("checked_food");
+		});
+
+		my_item.on('dblclick', function(){
+			$(this).fadeOut('slow');
+		});
+
+		if (my_text!=""){$("#wrap").append(my_item);}
+
+		else{alert("Type an item in before you add it.")}
+	});
+  
+  $(document).keypress(function(e){
+    if(e.which == 13){
+    	var my_text = $("#entry").val();
+			var my_item = $('<div class="food"></div>').text(my_text);
 
 			my_item.on('click', function(){
 				$(this).toggleClass("checked_food");
@@ -11,18 +28,29 @@ $(document).ready(function(){
 			my_item.on('dblclick', function(){
 				$(this).fadeOut('slow');
 			});
-		
-		$("#wrap").append(my_item);
+
+			if (my_text!="")
+			{
+				$("#wrap").append(my_item);
+			}
+
+			else 
+			{
+				alert("Type an item in before you add it.")
+			}
+    }
 	});
-
-		//this is so the first sample item behaves like the rest
-		$("div.food").click(function(){
-			$(this).toggleClass("checked_food");
-		});
-
-		$("div.food").dblclick(function(){
-			$(this).hide("slow");
-		});
 });
 
-//
+
+	
+
+	//this is so the first sample item behaves like the rest
+		//$("div.food").click(function(){
+			//$(this).toggleClass("checked_food");
+		//});
+
+		//$("div.food").dblclick(function(){
+			//$(this).hide("slow");
+		//});
+
